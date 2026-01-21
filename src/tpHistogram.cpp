991e1cae1e3b11f10bbf5303ef9f783bd16743c9
max <38135396+3v3r51nc3@users.noreply.github.com>
2026-01-22 00:35:09 +0100

    Update tpHistogram.cpp

--- src/tpHistogram.cpp
+++ src/tpHistogram.cpp
@@ -5,47 +5,6 @@
 using namespace cv;
 using namespace std;
 
-std::string type2str(int type)
-{
-    std::string r;
-
-    uchar depth = type & CV_MAT_DEPTH_MASK;
-    uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-    switch (depth)
-    {
-    case CV_8U:
-        r = "CV_8U";
-        break;
-    case CV_8S:
-        r = "CV_8S";
-        break;
-    case CV_16U:
-        r = "CV_16U";
-        break;
-    case CV_16S:
-        r = "CV_16S";
-        break;
-    case CV_32S:
-        r = "CV_32S";
-        break;
-    case CV_32F:
-        r = "CV_32F";
-        break;
-    case CV_64F:
-        r = "CV_64F";
-        break;
-    default:
-        r = "Unknown";
-        break;
-    }
-
-    r += "C";
-    r += std::to_string(chans);
-
-    return r;
-}
-
 /**
     Inverse a grayscale image with float values.
     for all pixel p: res(p) = 1.0 - image(p)
